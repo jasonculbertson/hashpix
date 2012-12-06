230a9c5779f7c8487a1eff4e6809d239ed168b4f
Jason <jculbertson@gmail.com>
2012-12-05 17:30:24 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashpix::Application.config.secret_token = 'e902ba1730b4426cf103b715cb8028e93bbadf29c812ead070b0abe859eba3dbfdee5c37975ebdcd8748c06f16ca216838e56aaa769c05a204bbe6b471bb1d6d'
